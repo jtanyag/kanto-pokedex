e771c8f26e21f1cc067d6c4c5643089f8798d668
Justin <jtanyag@gmail.com>
2026-08-16 08:59:58 -0700

    Update to fetch and display specific pokemon data

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,43 @@ function App() {
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0')
       .then(res => res.json())
-      .then(pokemonData => setKantoPokemon(pokemonData.results))
+      .then(pokemonData =>
+        Promise.all(
+          pokemonData.results.map(pokemon =>
+            fetch(pokemon.url)
+              .then(res => res.json())
+              .then(pokemonDetails => ({
+                id: pokemonDetails.id,
+                name: pokemonDetails.name,
+                image: pokemonDetails.sprites.front_default,
+                types: pokemonDetails.types
+              }))
+          )
+        )
+      )
+      .then(pokemonDetails => {
+        setKantoPokemon(pokemonDetails);
+      })
   }, []);
 
-  console.log(kantoPokemon);
+  const pokemonList = kantoPokemon.map(pokemon => {
+    console.log(pokemon.types)
+    const pokeTypes = pokemon.types.map(typeInfo => {
+      return (
+        <span key={typeInfo.type.name} className={`pokemon-type ${typeInfo.type.name}`}>
+          {typeInfo.type.name}
+        </span>
+      )
+    })
 
-  const pokemonList = kantoPokemon.map((pokemon, index) => {
     return (
-      <li key={index}>{pokemon.name}</li>
+      <div key={pokemon.id} className="pokemon-card">
+        <p>#{pokemon.id} {pokemon.name}</p>
+        <div className="pokemon-image">
+          <img src={pokemon.image} alt={pokemon.name} width="" height="" />
+        </div>
+        <p>Type(s): {pokeTypes}</p>
+      </div>
     )
   })
 
@@ -21,9 +50,7 @@ function App() {
     <main>
       <h1>Pokedex - Kanto</h1>
       <section>
-        <ul>
-          {pokemonList}
-        </ul>
+        {pokemonList}
       </section>
     </main>
   )
