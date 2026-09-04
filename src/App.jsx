7731433f755240c8ae0cfad632ae057116e8f379
Justin Tanyag <jtanyag@gmail.com>
2026-09-04 13:36:46 -0700

    Refactor fetch and add loading state

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,33 @@ import Buttons from './components/Buttons'
 function App() {
   const [kantoPokemon, setKantoPokemon] = useState([]);
   const [visibleCount, setVisibleCount] = useState(20);
+  const [loading, isLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0')
-      .then(res => res.json())
-      .then(pokemonData =>
-        Promise.all(
-          pokemonData.results.map(pokemon =>
-            fetch(pokemon.url)
-              .then(res => res.json())
-              .then(pokemonDetails => ({
-                id: pokemonDetails.id,
-                name: pokemonDetails.name,
-                image: pokemonDetails.sprites.front_default,
-                types: pokemonDetails.types
-              }))
-          )
-        )
-      )
-      .then(pokemonDetails => {
-        setKantoPokemon(pokemonDetails);
-      })
+    const fetchPokemon = async () => {
+      try {
+        const pokemonResponse = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0');
+        const pokemonData = await pokemonResponse.json();
+        const pokemonList = await Promise.all(
+          pokemonData.results.map(async pokemon => {
+            const detailsResponse = await fetch(pokemon.url);
+            const pokemonDetails = await detailsResponse.json();
+            return {
+              id: pokemonDetails.id,
+              name: pokemonDetails.name,
+              image: pokemonDetails.sprites.front_default,
+              types: pokemonDetails.types
+            };
+          })
+        );
+        setKantoPokemon(pokemonList);
+      } catch(error) {
+        console.error("Error fetching Pokemon: ", error);
+      } finally {
+        isLoading(false);
+      }
+    };
+    fetchPokemon();
   }, []);
 
   const handleLoadMore = () => {
@@ -33,10 +39,11 @@ function App() {
   }
 
   return (
-    <main className="h-screen py-10 px-6">
+    <main className="min-h-screen py-10 px-6">
       <h1 className="text-4xl text-center mb-8">Pokedex - Kanto</h1>
       <PokemonCardContainer
         pokemonList={kantoPokemon.slice(0, visibleCount)}
+        loading={loading}
       />
 
       <Buttons
--- src/components/PokemonCardContainer.jsx
+++ src/components/PokemonCardContainer.jsx
@@ -1,6 +1,11 @@
 import PokemonCard from "./PokemonCard"
 
 function PokemonCardContainer(props) {
+  if (props.loading) {
+    return (
+      <div className="text-center">Loading....</div>
+    )
+  }
   return (
     <section className="pokemon-cards bg-neutral p-8 rounded-md flex flex-wrap justify-center gap-4 max-w-7xl mx-auto">
       {props.pokemonList.map(pokemon => (
