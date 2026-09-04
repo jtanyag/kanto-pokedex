1b96b7d3107062d6ea2d89ef32cfad2cd53a803a
Justin Tanyag <jtanyag@gmail.com>
2026-09-04 14:31:09 -0700

    Refactor props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,7 @@
-function PokemonCard(props) {
-  const pokeTypes = props.pokemon.types.map(typeInfo => {
+function PokemonCard({pokemon}) {
+  const {types, name, id, image} = pokemon;
+
+  const pokeTypes = types.map(typeInfo => {
     const capTypeName = typeInfo.type.name.charAt(0).toUpperCase() + typeInfo.type.name.slice(1);
     return (
       <span key={capTypeName} className={`pokemon-type ${typeInfo.type.name} rounded-md text-center w-31.25 p-1`}>
@@ -7,13 +9,13 @@ function PokemonCard(props) {
       </span>
     )
   })
-  const capName = props.pokemon.name.charAt(0).toUpperCase() + props.pokemon.name.slice(1);
+  const capName = name.charAt(0).toUpperCase() + name.slice(1);
 
   return (
-    <div key={props.pokemon.id} className="pokemon-card bg-zinc-100 rounded-md text-xs w-40 p-4">
-      <p>#{props.pokemon.id}<br />{capName}</p>
+    <div className="pokemon-card bg-zinc-100 rounded-md text-xs w-40 p-4">
+      <p>#{id}<br />{capName}</p>
       <div className="pokemon-image">
-        <img src={props.pokemon.image} alt={capName} width="" height="" className="h-12.5 object-contain w-full" />
+        <img src={image} alt={capName} width="" height="" className="h-12.5 object-contain w-full" />
       </div>
       <p>Type(s): <span className="pokemon-types flex flex-col justify-center items-center gap-2 mt-2 text-neutral-700">{pokeTypes}</span></p>
     </div>
--- src/components/PokemonCardContainer.jsx
+++ src/components/PokemonCardContainer.jsx
@@ -1,14 +1,15 @@
 import PokemonCard from "./PokemonCard"
 
-function PokemonCardContainer(props) {
-  if (props.loading) {
+function PokemonCardContainer({loading, pokemonList}) {
+  if (loading) {
     return (
       <div className="text-center">Loading....</div>
     )
   }
+  
   return (
     <section className="pokemon-cards bg-neutral p-8 rounded-md flex flex-wrap justify-center gap-4 max-w-7xl mx-auto">
-      {props.pokemonList.map(pokemon => (
+      {pokemonList.map(pokemon => (
         <PokemonCard
           key={pokemon.id}
           pokemon={pokemon}
